b9ac6778344c0a941ebf85e63042a87d2038f2a3
Md Sarwar Jahan Sabit <ssarwarjahan@gmail.com>
2020-01-11 14:35:35 +0600

    Update main.py
    Added reaction, normal image and gif support.

--- main.py
+++ main.py
@@ -25,8 +25,8 @@ def writeLogs(content, threadName, date):
         os.makedirs("message_logs/" + threadName)
     except FileExistsError:
         pass
-    
-    
+
+
     filename = "message_logs/" + threadName + "/" + str(date.day) + "_" + str(date.month) + "_" + str(date.year)
 
     if os.path.exists(filename):
@@ -34,46 +34,73 @@ def writeLogs(content, threadName, date):
         content = "\n" + content
     else:
         append_write = 'w' # make a new file if not
-    
-    
+
+
     with open(filename, mode = append_write, encoding='utf8') as write_in:
         write_in.write(content)
-    
+
     #write_in = open(filename, append_write)
-    
+
     write_in.close()
 
 class CustomClient(Client):
     def onMessage(self, author_id, message_object, thread_id, thread_type, ts, metadata, msg, **kwargs):
+       # print(message_object.attachments)
+        date = datetime.now()
+        user = self.fetchUserInfo(author_id)[author_id]
+        thread = self.fetchThreadInfo(thread_id)[thread_id]
+        #print(len(message_object.attachments))
+        #print("\n")
+        #print(message_object)
+        #print(message_object.attachments[0].large_preview_url)
+        
         text = message_object.text
-        if message_object.text == None:
+        
+        if message_object.text == None and message_object.sticker == None and message_object.attachments[0].large_preview_url == None:
+            for i in range(0, len(message_object.attachments)):
+                #print(message_object.attachments[i].animated_preview_url)
+                try:
+                    content = "{}: {}".format(user.name, str(message_object.attachments[i].animated_preview_url))
+                    bot.sendMessage(bot_chat_id, "### " + thread.name + " ###" + "\n" + content) #DTA
+                    writeLogs(time.ctime() + " | " + content, thread.name, date)
+                except Exception as E:
+                    traceback.print_exc()        
+        elif message_object.text == None and message_object.sticker == None:
+            for i in range(0, len(message_object.attachments)):
+                #print(message_object.attachments[i].large_preview_url)
+                try:
+                    content = "{}: {}".format(user.name, str(message_object.attachments[i].large_preview_url))
+                    bot.sendMessage(bot_chat_id, "### " + thread.name + " ###" + "\n" + content) #DTA
+                    writeLogs(time.ctime() + " | " + content, thread.name, date)
+                except Exception as E:
+                    traceback.print_exc()
+        elif message_object.text == None:
             get_url = str(message_object.sticker)
             index1 = get_url.find("url")
             index1 += 5
             index2 = get_url.find("', width=240")
             text = get_url[index1:index2]
-            print(get_url[index1:index2])
-        
-        date = datetime.now()
-        try:
-            user = self.fetchUserInfo(author_id)[author_id]
-            thread = self.fetchThreadInfo(thread_id)[thread_id]
+            #print(get_url[index1:index2])
             content = "{}: {}".format(user.name, text)
             bot.sendMessage(bot_chat_id, "### " + thread.name + " ###" + "\n" + content) #DTA
-            writeLogs(time.ctime() + " | " + content, thread.name, date)
-            return
-        except:
-            thread = self.fetchThreadInfo(thread_id)[thread_id]
-            content = "{}: {}".format(thread.name, text)
-            bot.sendMessage(bot_chat_id, "### " + thread.name + " ###" + "\n" + content) #DTA
-            writeLogs(time.ctime() + " | " + content, thread.name, date)
-            return
+            writeLogs(time.ctime() + " | " + content, thread.name, date)        
         else:
-            pass
-    
+            try:
+                content = "{}: {}".format(user.name, text)
+                bot.sendMessage(bot_chat_id, "### " + thread.name + " ###" + "\n" + content) #DTA
+                writeLogs(time.ctime() + " | " + content, thread.name, date)
+                return
+            except:
+                content = "{}: {}".format(thread.name, text)
+                bot.sendMessage(bot_chat_id, "### " + thread.name + " ###" + "\n" + content) #DTA
+                writeLogs(time.ctime() + " | " + content, thread.name, date)
+                return
+            else:
+                pass
+
     def onReactionAdded(self, mid, reaction, author_id, thread_id, thread_type, ts, msg, **kwargs):
         date = datetime.now()
-        
+
         user = self.fetchUserInfo(author_id)[author_id]
         thread = self.fetchThreadInfo(thread_id)[thread_id]
         x = self.fetchMessageInfo(mid, thread_id)
