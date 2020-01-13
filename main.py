5d4729b37a18dd8e35ada866d3deac2e68a4ca20
Md Sarwar Jahan Sabit <ssarwarjahan@gmail.com>
2020-01-14 02:33:49 +0600

    Update main.py
    Has simplified code somehow ;-p
    Added one-key (actually a variable ;-p ) to turn on/off Telegram usage. Added better support for receiving images, gifs, attachments, stickers and multi file at a time. Also added response for when reaction removed and better response for when reaction added.
    Lots of comments!
    Cookies function not working!

--- main.py
+++ main.py
@@ -1,47 +1,95 @@
-'''
-If you don't want to use Telegram,
-delete or comment out lines containing "#DTA"
- Otherwise, you'll get error!
-
-'''
-
-
-from fbchat import Client
-from fbchat.models import *
 import os
 import time
 from datetime import datetime
-import telepot #DTA
+import traceback
+import json
+from fbchat import Client
+from fbchat.models import *
 
 
-email = 'ajanta@gmail.com' #Facebook Email
-password = 'qwerty1234567*' #Facebook Password
+email = 'randomemail1@yahoo.com' #Facebook Email
+password = 'zxxcqwer1234zx' #Facebook Password
+USE_TELEGRAM = True #Change to True if you don't want to use Telegram
 
-bot = telepot.Bot('0000000000000000:dufyfwe87y7eydy87344444444444444444444444444y') # Telegram Bot Token #DTA
-bot_chat_id = '123456789' # Your Telegram Chat ID. It can be a user's chat ID or a group's chat ID. #DTA
+if USE_TELEGRAM == True:
+    import telepot 
+    bot = telepot.Bot('00000000:45h5t78574t45h7485th7857h45y45t78h45t') # Telegram Bot Token 
+    bot_chat_id = '111111111' # Your Telegram Chat ID. It can be a user's chat ID or a group's chat ID. 
+    
 
 def writeLogs(content, threadName, date):
+
     try:
         os.makedirs("message_logs/" + threadName)
     except FileExistsError:
         pass
 
-
     filename = "message_logs/" + threadName + "/" + str(date.day) + "_" + str(date.month) + "_" + str(date.year)
 
     if os.path.exists(filename):
         append_write = 'a' # append if already exists
-        content = "\n" + content
+        #content = "\n" + content
     else:
         append_write = 'w' # make a new file if not
 
-
     with open(filename, mode = append_write, encoding='utf8') as write_in:
         write_in.write(content)
-
     #write_in = open(filename, append_write)
-
     write_in.close()
+    
+    if USE_TELEGRAM == True:
+        bot.sendMessage(bot_chat_id, "### " + threadName + " ###" + "\n" + content) 
+
+
+
+
+def getMessageContent(self, t, messageObject):
+    #if len(message_object.attachments) != 0:
+    # Checking if it is gif
+    if messageObject.text == None and messageObject.sticker == None:
+        for i in range(0, len(messageObject.attachments)):
+            #print(messageObject.attachments[i].animated_preview_url)
+            #try: #If == ImageType
+            if hasattr(messageObject.attachments[i], 'animated_preview_url') and messageObject.attachments[i].animated_preview_url != None:
+                #if messageObject.attachments[i].animated_preview_url != None: #If gif
+                url = self.fetchImageUrl(messageObject.attachments[i].uid)
+                t += "\nGif: " + url
+                #t+= str(messageObject.attachments[i].animated_preview_url) + "\n\n"
+                #print("Gif               " +t)
+                # content = "{}: {}".format(user.name, str(messageObject.attachments[i].animated_preview_url))
+                # bot.sendMessage(bot_chat_id, "### " + thread.name + " ###" + "\n" + content) 
+                # writeLogs(time.ctime() + " | " + content, thread.name, date)
+            elif hasattr(messageObject.attachments[i], 'large_preview_url') and messageObject.attachments[i].large_preview_url != None:
+                #elif messageObject.attachments[i].large_preview_url != None: #If normal image
+                url = self.fetchImageUrl(messageObject.attachments[i].uid)
+                t += "\nImage: " + url
+                #t+= str(messageObject.attachments[i].large_preview_url) + "\n\n"
+                #print("Image               " +t)
+            elif hasattr(messageObject.attachments[i], 'url') and messageObject.attachments[i].url != None:
+                #else: #If file rather than image (i.e. pdf, docx etc)
+                #if messageObject.attachments[i].url != None:
+                t += "\nFile: " + str(messageObject.attachments[i].url) + "\n\n"
+                #print("File               " +t)
+    
+    # Checking if it is sticker (like button or usual sticker)
+    elif messageObject.sticker != None:
+        get_url = str(messageObject.sticker)
+        index1 = get_url.find("url")
+        index1 += 5
+        index2 = get_url.find("', width=")
+        t = "\nSticker: " + get_url[index1:index2]
+        #print(get_url[index1:index2])
+        #content = "{}: {}".format(user.name,t)
+        #bot.sendMessage(bot_chat_id, "### " + thread.name + " ###" + "\n" + content) 
+        #writeLogs(time.ctime() + " | " + content, thread.name, date)
+    elif messageObject.text != None:
+        t = messageObject.text
+    
+    return t
+
+
+
+
 
 class CustomClient(Client):
     def onMessage(self, author_id, message_object, thread_id, thread_type, ts, metadata, msg, **kwargs):
@@ -51,52 +99,17 @@ class CustomClient(Client):
         thread = self.fetchThreadInfo(thread_id)[thread_id]
         #print(len(message_object.attachments))
         #print("\n")
-        #print(message_object)
+        print(str(message_object) + "\n\n\n")
         #print(message_object.attachments[0].large_preview_url)
         
-        text = message_object.text
+        text = ''
         
-        if message_object.text == None and message_object.sticker == None and message_object.attachments[0].large_preview_url == None:
-            for i in range(0, len(message_object.attachments)):
-                #print(message_object.attachments[i].animated_preview_url)
-                try:
-                    content = "{}: {}".format(user.name, str(message_object.attachments[i].animated_preview_url))
-                    bot.sendMessage(bot_chat_id, "### " + thread.name + " ###" + "\n" + content) #DTA
-                    writeLogs(time.ctime() + " | " + content, thread.name, date)
-                except Exception as E:
-                    traceback.print_exc()        
-        elif message_object.text == None and message_object.sticker == None:
-            for i in range(0, len(message_object.attachments)):
-                #print(message_object.attachments[i].large_preview_url)
-                try:
-                    content = "{}: {}".format(user.name, str(message_object.attachments[i].large_preview_url))
-                    bot.sendMessage(bot_chat_id, "### " + thread.name + " ###" + "\n" + content) #DTA
-                    writeLogs(time.ctime() + " | " + content, thread.name, date)
-                except Exception as E:
-                    traceback.print_exc()
-        elif message_object.text == None:
-            get_url = str(message_object.sticker)
-            index1 = get_url.find("url")
-            index1 += 5
-            index2 = get_url.find("', width=240")
-            text = get_url[index1:index2]
-            #print(get_url[index1:index2])
-            content = "{}: {}".format(user.name, text)
-            bot.sendMessage(bot_chat_id, "### " + thread.name + " ###" + "\n" + content) #DTA
-            writeLogs(time.ctime() + " | " + content, thread.name, date)        
-        else:
-            try:
-                content = "{}: {}".format(user.name, text)
-                bot.sendMessage(bot_chat_id, "### " + thread.name + " ###" + "\n" + content) #DTA
-                writeLogs(time.ctime() + " | " + content, thread.name, date)
-                return
-            except:
-                content = "{}: {}".format(thread.name, text)
-                bot.sendMessage(bot_chat_id, "### " + thread.name + " ###" + "\n" + content) #DTA
-                writeLogs(time.ctime() + " | " + content, thread.name, date)
-                return
-            else:
-                pass
+        text = getMessageContent(self, text, message_object)
+      
+        content = "*{}*: {}".format(user.name, text)
+        #bot.sendMessage(bot_chat_id, "### " + thread.name + " ###" + "\n" + content) 
+        writeLogs(time.ctime() + " | " + content, thread.name, date)
+        return
 
     def onReactionAdded(self, mid, reaction, author_id, thread_id, thread_type, ts, msg, **kwargs):
         date = datetime.now()
@@ -105,15 +118,58 @@ class CustomClient(Client):
         thread = self.fetchThreadInfo(thread_id)[thread_id]
         x = self.fetchMessageInfo(mid, thread_id)
         y = self.fetchUserInfo(x.author)[x.author]
-        mess = x.text
+        #mess = x.text
         
-        if mess == None or mess == "":
-            mess = "%Sticker, image or link%"
-        content = "{} reacted {} to message ** {}: {} **".format(user.name, reaction.name, y.name, mess)
-        bot.sendMessage(bot_chat_id, "### " + thread.name + " ###" + "\n" + content) #DTA
+        '''if mess == None or mess == "":
+            mess = "%Sticker, image or link%"'''
+        
+        text = ''
+        text = getMessageContent(self, text, x)
+        
+        
+        content = "{} reacted {} to message * {}: {} *".format(user.name, reaction.name, y.name, text)
+        #bot.sendMessage(bot_chat_id, "### " + thread.name + " ###" + "\n" + content) 
         writeLogs(time.ctime() + " | " + content, thread.name, date)
+        
+    def onReactionRemoved(self, mid, author_id, thread_id, thread_type, ts, msg, **kwargs):
+        date = datetime.now()
 
-client = CustomClient(email, password)
-session = client.getSession()
+        user = self.fetchUserInfo(author_id)[author_id]
+        thread = self.fetchThreadInfo(thread_id)[thread_id]
+        x = self.fetchMessageInfo(mid, thread_id)
+        y = self.fetchUserInfo(x.author)[x.author]
+        #mess = x.text
+        
+        '''if mess == None or mess == "":
+            mess = "%Sticker, image or link%"'''
+        text = ''
+        text = getMessageContent(self, text, x)
+        print(msg)
+        content = "{} removed reaction of message * {}: {} *".format(user.name, y.name, text)
+        #bot.sendMessage(bot_chat_id, "### " + thread.name + " ###" + "\n" + content) 
+        writeLogs(time.ctime() + " | " + content, thread.name, date)
+        
+    def onColorChange( self, mid, author_id, new_color, thread_id, thread_type, ts, metadata, msg, **kwargs):
+        date = datetime.now()
+        user = self.fetchUserInfo(author_id)[author_id]
+        thread = self.fetchThreadInfo(thread_id)[thread_id]
+        
+        content = "Conversation Theme changed to {} by {}".format(new_color, user.name)
+        writeLogs(time.ctime() + " | " + content, thread.name, date)
+        
+
+try:
+    with open("session.json") as f:
+        cookies = json.load(f)
+    client = fbchat.Client(USER, PASSWORD, session_cookies=cookies)
+    print('Logging in using cookies...')
+except:
+    print('Cookies not found!')
+    client = CustomClient(email, password)
+    cookies = client.getSession()
+    with open("session.json", "w+") as f:
+        json.dump(cookies, f)
+    print('Cookies saved!')
+
 print("\nProgram Started!\n")
 client.listen()
