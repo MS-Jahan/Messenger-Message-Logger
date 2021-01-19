8f430167a030b11fe973fd2003c3f94633096743
Md Sarwar Jahan Sabit <ssarwarjahan@gmail.com>
2021-01-19 13:11:42 +0600

    Update main.py

--- main.py
+++ main.py
@@ -10,6 +10,11 @@ from fbchat import FBchatException, FBchatUserError
 start = time.time()
 end = 0
 
+import fbchat
+import re
+fbchat._util.USER_AGENTS    = ["Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.75 Safari/537.36"]
+fbchat._state.FB_DTSG_REGEX = re.compile(r'"name":"fb_dtsg","value":"(.*?)"')
+
 
 email = 'randomemail1@yahoo.com' #Facebook Email
 password = 'zxxcqwer1234zx' #Facebook Password
